09474068f1f254ba025a5967e6c210782e8d04b6
sashreek1 <45600974+sashreek1@users.noreply.github.com>
2019-12-05 09:21:44 +0530

    changed timeout value to increase speed

--- wifi_scan.py
+++ wifi_scan.py
@@ -5,7 +5,7 @@ def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered_list = scapy.srp(arp_request_broadcast, timeout=1,verbose=False)[0]
+    answered_list = scapy.srp(arp_request_broadcast, timeout=0.25,verbose=False)[0]
 
     clients_list = []
     for element in answered_list:
